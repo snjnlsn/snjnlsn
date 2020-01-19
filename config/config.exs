8b39751e617303bbbccd57e30bcc243bbb52ddfa
Sanjay Nelson <sanjay.nelson@kroger.com>
2020-01-19 14:04:13 -0500

    trying to fix auth

--- config/config.exs
+++ config/config.exs
@@ -30,4 +30,6 @@ config :phoenix, :json_library, Jason
 
 # Import environment specific config. This must remain at the bottom
 # of this file so it overrides the configuration defined above.
+import_config "secret.exs"
+import_config "spotify.exs"
 import_config "#{Mix.env()}.exs"
