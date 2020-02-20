e40590651b6e9681788fcb456cc62b60ccb0f27e
Sanjay Nelson <sanjay.nelson@kroger.com>
2020-02-19 23:34:32 -0500

    move Playlist fetching into context

--- lib/bulma_time/playlists.ex
+++ lib/bulma_time/playlists.ex
@@ -5,11 +5,20 @@ defmodule BulmaTime.Playlists do
 
   alias BulmaTime.Playlists.Playlist
 
+  defstruct items: [%Playlist{}]
+
   @doc """
   Returns list of Sanjay's playlists
   """
-  @spec load() :: [%Playlist{}]
-  def load do
-    [%Playlist{}]
+  @spec load(term()) :: __MODULE__.t()
+  def load(token) do
+    case Playlist.fetch_playlists(token) do
+      {:ok, playlists} ->
+        playlists
+
+      {:error, reason} ->
+        IO.puts("Error fetching playlist: ", reason)
+        [%Playlist{}]
+    end
   end
 end
--- lib/bulma_time/playlists/playlist.ex
+++ lib/bulma_time/playlists/playlist.ex
@@ -3,6 +3,9 @@ defmodule BulmaTime.Playlists.Playlist do
   Private logic for handling Spotify Playlists, to be used by public Playlists context
   """
 
+  require IEx
+  @playlist_url "https://api.spotify.com/v1/users/smwus5mq52q7u9zymllzghwyr/playlists"
+
   defstruct name: "",
             description: "",
             id: "",
@@ -10,7 +13,8 @@ defmodule BulmaTime.Playlists.Playlist do
             images: %{},
             tracks: %{},
             snapshot_id: "",
-            current: false
+            current: false,
+            public: false
 
   @type t() :: %__MODULE__{
           name: String.t(),
@@ -20,8 +24,21 @@ defmodule BulmaTime.Playlists.Playlist do
           images: map(),
           tracks: map(),
           snapshot_id: String.t(),
-          current: boolean()
+          current: boolean(),
+          public: boolean()
         }
 
-  def fetch_playlists, do: [%__MODULE__{}, %__MODULE__{}]
+  def fetch_playlists(token) do
+    case HTTPoison.get(@playlist_url,
+           Authorization: "Bearer #{token}"
+         ) do
+      {:ok, response} ->
+        {:ok,
+         Poison.decode!(response.body, as: %BulmaTime.Playlists{}).items
+         |> Enum.filter(fn p -> p.public end)}
+
+      {:error, reason} ->
+        {:error, reason}
+    end
+  end
 end
