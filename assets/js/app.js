67dc01d3598affacb5b61c1c60adaf17d0088c69
Sanjay Nelson <services@snjnlsn.co>
2021-02-20 21:31:35 -0500

    this has working recording implementation :)

--- assets/js/app.js
+++ assets/js/app.js
@@ -13,10 +13,94 @@ import "phoenix_html"
 import { Socket } from "phoenix"
 import LiveSocket from "phoenix_live_view"
 
-console.log(document, window)
-if ("MediaRecorder" in window) {
-  // everything is good, let's go ahead
-  alert("good")
+function renderError(message) {
+  const wrapper = document.getElementById("errorMsg")
+  wrapper.innerHTML = `<div class="error"><p>${message}</p></div>`
+}
+
+function renderRecording(blob, list) {
+  const blobUrl = URL.createObjectURL(blob)
+  const li = document.createElement("li")
+  const audio = document.createElement("audio")
+  const anchor = document.createElement("a")
+  anchor.setAttribute("href", blobUrl)
+  const now = new Date()
+  anchor.setAttribute(
+    "download",
+    `recording-${now.getFullYear()}-${(now.getMonth() + 1)
+      .toString()
+      .padStart(2, "0")}-${now
+      .getDay()
+      .toString()
+      .padStart(2, "0")}--${now
+      .getHours()
+      .toString()
+      .padStart(2, "0")}-${now
+      .getMinutes()
+      .toString()
+      .padStart(2, "0")}-${now.getSeconds().toString().padStart(2, "0")}.webm`
+  )
+  anchor.innerText = "Download"
+  audio.setAttribute("src", blobUrl)
+  audio.setAttribute("controls", "controls")
+  li.appendChild(audio)
+  li.appendChild(anchor)
+  list.appendChild(li)
+}
+
+const mic = document.getElementById("mic")
+
+let audioCtx, audioNode
+if ("MediaRecorder" in window && mic) {
+  const recordButton = document.getElementById("record"),
+    list = document.getElementById("recordings"),
+    codeBlock = document.getElementById("code-block")
+
+  mic.addEventListener("click", async () => {
+    try {
+      const stream = await navigator.mediaDevices.getUserMedia({
+        audio: {
+          sampleRate: 48000,
+          echoCancellation: false,
+          noiseSuppression: false,
+        },
+        video: false,
+      })
+      const mimeType = "audio/aac"
+      const chunks = []
+      const recorder = new MediaRecorder(stream, {
+        type: mimeType,
+      })
+      recorder.addEventListener("dataavailable", (event) => {
+        if (typeof event.data === "undefined") return
+        if (event.data.size === 0) return
+        chunks.push(event.data)
+      })
+      recorder.addEventListener("stop", () => {
+        const recording = new Blob(chunks, {
+          type: mimeType,
+        })
+        renderRecording(recording, list)
+        chunks = []
+      })
+      recordButton.removeAttribute("hidden")
+      recordButton.addEventListener("click", () => {
+        if (recorder.state === "inactive") {
+          recorder.start()
+          recordButton.innerText = "Stop"
+        } else {
+          recorder.stop()
+          renderRecording(recording, list)
+          recordButton.innerText = "Record"
+        }
+      })
+      // codeBlock.innerHTML = stream
+    } catch {
+      renderError(
+        "You denied access to the microphone so this demo will not work."
+      )
+    }
+  })
 } else {
   alert("bad")
 }
@@ -24,47 +108,46 @@ if ("MediaRecorder" in window) {
 const Hooks = {
   StartCamera: {
     mounted() {
-      console.log(document, window)
-      let hook = this
-      const audio = this.el
-      navigator.mediaDevices
-        .getUserMedia({
-          audio: true,
-          video: false,
-        })
-        .then((stream) => {
-          audio.srcObject = stream
-          audio.onloadedmetadata = (e) => {
-            audio.play()
-            const mediaRecorder = new MediaRecorder(stream, {
-              mimeType: "video/webm",
-              videoBitsPerSecond: 3000000,
-            })
-            mediaRecorder.ondataavailable = (e) => {
-              const reader = new FileReader()
-              reader.onloadend = () => {
-                hook.pushEvent("video_data", { data: reader.result })
-              }
-              reader.readAsDataURL(e.data)
-            }
-            mediaRecorder.start(1000)
-          }
-        })
+      // console.log(document, window)
+      // let hook = this
+      // const audio = this.el
+      // navigator.mediaDevices
+      //   .getUserMedia({
+      //     audio: true,
+      //     video: false,
+      //   })
+      //   .then((stream) => {
+      //     audio.srcObject = stream
+      //     audio.onloadedmetadata = (e) => {
+      //       audio.play()
+      //       const mediaRecorder = new MediaRecorder(stream, {
+      //         mimeType: "video/webm",
+      //         videoBitsPerSecond: 3000000,
+      //       })
+      //       mediaRecorder.ondataavailable = (e) => {
+      //         const reader = new FileReader()
+      //         reader.onloadend = () => {
+      //           hook.pushEvent("video_data", { data: reader.result })
+      //         }
+      //         reader.readAsDataURL(e.data)
+      //       }
+      //       mediaRecorder.start(1000)
+      //     }
+      //   })
     },
   },
   StartMemo: {
     mounted() {
-
       /* i have to figure out how to make his synchronous in order for it to work as a hook (which i think is the move? maybe not) */
-      let hook = this;
-      console.log('hello')
-      const audio = this.el
-      if ('MediaRecorder' in window) {
-        const stream = await navigator.mediaDevices.getUserMedia({ audio: true, video: false})
-        console.log(stream)
-      }
-    }
-  }
+      //   let hook = this;
+      //   console.log('hello')
+      //   const audio = this.el
+      //   if ('MediaRecorder' in window) {
+      //     const stream = await navigator.mediaDevices.getUserMedia({ audio: true, video: false})
+      //     console.log(stream)
+      //   }
+    },
+  },
 }
 
 const csrfToken = document
